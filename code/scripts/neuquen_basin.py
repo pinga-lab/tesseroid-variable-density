a938f3c69345d18213869db2245d903224cd8b23
Santiago Soler <santiago.r.soler@gmail.com>
2018-12-18 16:17:02 -0300

    Start writing neuquen basin plots

--- code/scripts/neuquen_basin.py
+++ code/scripts/neuquen_basin.py
@@ -1,11 +1,14 @@
 from __future__ import division
 import os
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 from matplotlib.patches import Rectangle
 from matplotlib.colors import LightSource
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from matplotlib.gridspec import GridSpec
 from fatiando import gridder
 from tesseroid_density import tesseroid
 from tesseroid_model import TesseroidModel
@@ -154,121 +157,184 @@ for field in fields:
              height=grid["height"], shape=grid["shape"])
 
 
+# ------------
 # Plot Results
 # ------------
+
+# Configure LaTeX style for plots
+# -------------------------------
+try:
+    plt.rcParams['text.usetex'] = True
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = 'Computer Modern Roman'
+    plt.rcParams['xtick.major.size'] = 2
+    plt.rcParams['ytick.major.size'] = 2
+except Exception as e:
+    warnings.warn("Couldn't configure LaTeX style for plots:" + str(e))
+
+# Create basemap and configuration
+# --------------------------------
+bm = Basemap(projection='merc',
+             llcrnrlon=topography["area"][2],
+             llcrnrlat=topography["area"][0],
+             urcrnrlon=topography["area"][3],
+             urcrnrlat=topography["area"][1],
+             resolution='i')
+
+config = {'countries': dict(linewidth=0.5, color='k'),
+          'states': dict(linewidth=0.4, linestyle='--', color='k'),
+          'coastlines': dict(linewidth=0.5, color='k'),
+          'meridians': dict(meridians=np.arange(-80, -50, 2),
+                            linewidth=0.5,
+                            labels=[False, False, True, False],
+                            labelstyle='+/-'),
+          'parallels': dict(circles=np.arange(-50, -30, 2),
+                            linewidth=0.5,
+                            labels=[True, False, False, False],
+                            labelstyle='+/-')}
+config['parallels-quiet'] = config['parallels'].copy()
+config['parallels-quiet']['labels'] = [False, False, False, False]
+
+config['meridians-quiet'] = config['meridians'].copy()
+config['meridians-quiet']['labels'] = [False, False, False, False]
+
+config['parallels-right'] = config['parallels'].copy()
+config['parallels-right']['labels'] = [False, True, False, False]
+
+
+# Create outer grid
+fig = plt.figure(figsize=(6.66, 8))
+grid = GridSpec(ncols=2, nrows=2, wspace=0.001, hspace=0.1)
+
+
+# Topography
+# ----------
+ax = plt.subplot(grid[0, 0])
+bm.ax = ax
+ax.set_title("(a)", y=1.08, loc='left')
+ax.set_title("Neuquen Basin", y=1.08, loc="center")
+x, y = bm(topography['lon'], topography['lat'])
+shape = topography['shape']
+vmax = np.abs([np.nanmin(topography['topo']),
+               np.nanmax(topography['topo'])]).max()
+vmin = -vmax
+cmap = plt.cm.gist_earth
+
+# Hillshaded topography
+ls = LightSource(azdeg=120)
+rgb = ls.shade(topography['topo'].reshape(shape),
+               cmap, blend_mode='soft', vert_exag=1000,
+               vmin=vmin, vmax=vmax)
+bm.imshow(rgb)
+
+# Proxy image for colorbar
+im = bm.imshow(1e-3*topography['topo'].reshape(shape), cmap=cmap)
+im.remove()
+cbar = bm.colorbar(im, location='bottom', pad="3%")
+tick_locator = ticker.MaxNLocator(nbins=5)
+cbar.locator = tick_locator
+cbar.update_ticks()
+cbar.ax.tick_params(labelsize=9)
+
+# Basemap configuration
+bm.drawcountries(**config['countries'])
+bm.drawstates(**config['states'])
+bm.drawcoastlines(**config['coastlines'])
+line = bm.drawmeridians(**config['meridians'])
+line = bm.drawparallels(**config['parallels'])
+
+# Location map
+height, width = "50%", "29.375%"
+axins = inset_axes(ax,
+                   width=width,
+                   height=height,
+                   loc=1,
+                   borderpad=0)
+bm2 = Basemap(projection='merc',
+              llcrnrlon=360 - 75, llcrnrlat=-56,
+              urcrnrlon=360 - 53, urcrnrlat=-21,
+              resolution='i', ax=axins)
+bm2.drawmapboundary(fill_color='#7777ff')
+bm2.fillcontinents(color='#ddaa66', lake_color='#7777ff')
+bm2.drawcountries(linewidth=0.02)
+bm2.drawcoastlines(linewidth=0.02)
+x1, y1 = bm2(area[2], area[0])
+x2, y2 = bm2(area[3], area[1])
+rectangle = Rectangle((x1, y1),
+                      abs(x2 - x1),
+                      abs(y2 - y1),
+                      facecolor='C3')
+axins.add_patch(rectangle)
+
+
+# Basin thickness
+# ---------------
+ax = plt.subplot(grid[0, 1])
+bm.ax = ax
+ax.set_title("(b)", y=1.08, loc='left')
+ax.set_title("Basin Thickness", y=1.08, loc="center")
+x, y = bm(sediments['lon'], sediments['lat'])
+im = bm.contourf(x.reshape(sediments['shape']),
+                 y.reshape(sediments['shape']),
+                 sediments['thickness'].reshape(sediments['shape']),
+                 100)
+bm.contour(x.reshape(sediments['shape']),
+           y.reshape(sediments['shape']),
+           sediments['thickness'].reshape(sediments['shape']),
+           5, colors='k', linewidths=0.7)
+bm.drawcountries(**config['countries'])
+bm.drawstates(**config['states'])
+bm.drawcoastlines(**config['coastlines'])
+bm.drawparallels(**config['parallels-quiet'])
+bm.drawmeridians(**config['meridians'])
+
+# Colorbar
+cbar = bm.colorbar(im, location='bottom', pad="3%")
+cbar.ax.tick_params(labelsize=9)
+tick_locator = ticker.MaxNLocator(nbins=5)
+cbar.locator = tick_locator
+cbar.update_ticks()
+
+
+# Results (exponential)
+# ---------------------
 labels = {"potential": "J/kg", "gz": "mGal"}
 titles = {"potential": r"$V$", "gz": r"$g_{z}$"}
-densities = ["homogeneous", "linear", "exponential"]
+fig_titles = {"potential": "(c)", "gz": "(d)"}
+grid_specs = {"potential": grid[1, 0], "gz": grid[1, 1]}
 
-for density in densities:
-    fig, axes = plt.subplots(1, 2, figsize=(15, 7))
-    axes = axes.ravel()
-    for field, ax in zip(fields, axes):
+density = "exponential"
+for field in fields:
+    ax = plt.subplot(grid_specs[field])
+    bm.ax = ax
+    ax.set_title(fig_titles[field], y=1.08, loc='left')
+    ax.set_title(titles[field], y=1.08, loc='center')
 
-        bm = Basemap(projection='merc',
-                     llcrnrlon=grid["area"][2],
-                     llcrnrlat=grid["area"][0],
-                     urcrnrlon=grid["area"][3],
-                     urcrnrlat=grid["area"][1],
-                     resolution='l', ax=ax)
+    # Plot result
+    result = np.load(os.path.join(result_dir, "{}-{}.npz".format(density, field)))
 
-        result = np.load(os.path.join(result_dir, "{}-{}.npz".format(density, field)))
+    shape = result["shape"]
+    im = bm.contourf(result["lon"].reshape(shape), result["lat"].reshape(shape),
+                     result["result"].reshape(shape), 100, latlon=True)
+    bm.contour(result["lon"].reshape(shape), result["lat"].reshape(shape),
+               result["result"].reshape(shape), 5, colors='k', linewidths=0.7,
+               latlon=True)
 
-        x, y = bm(result["lon"], result["lat"])
-        shape = result["shape"]
+    # Configure basemap
+    bm.drawcountries(**config['countries'])
+    bm.drawstates(**config['states'])
+    bm.drawcoastlines(**config['coastlines'])
+    bm.drawparallels(**config['parallels'])
+    bm.drawmeridians(**config['meridians'])
+    # bm.drawparallels(**config['parallels-quiet'])
+    # bm.drawmeridians(**config['meridians-quiet'])
 
-        cb = bm.contourf(x.reshape(shape), y.reshape(shape),
-                         result["result"].reshape(shape), 100)
-        bm.contour(x.reshape(shape), y.reshape(shape), result["result"].reshape(shape),
-                   5, colors='k', linewidths=0.7)
-        bm.drawcountries()
-        bm.drawstates()
-        bm.drawcoastlines()
-        bm.drawmeridians(np.arange(-80, -50, 2),
-                         labels=[False, False, False, True])
-        bm.drawparallels(np.arange(-50, -30, 2),
-                         labels=[True, False, False, False])
-        plt.colorbar(cb, ax=ax, label=labels[field])
-        ax.set_title(titles[field])
+    # Colorbar
+    cbar = bm.colorbar(im, location='bottom', pad="3%")
+    cbar.ax.tick_params(labelsize=9)
+    tick_locator = ticker.MaxNLocator(nbins=5)
+    cbar.locator = tick_locator
+    cbar.update_ticks()
 
-    plt.show()
-
-
-# Plot differences
-# ----------------
-labels = {"potential": "J/kg", "gz": "mGal"}
-titles = {"potential": r"$V$", "gz": r"$g_{z}$"}
-
-# Linear and Homogeneous
-fig, axes = plt.subplots(1, 2, figsize=(15, 7))
-axes = axes.ravel()
-for field, ax in zip(fields, axes):
-
-    bm = Basemap(projection='merc',
-                 llcrnrlon=grid["area"][2],
-                 llcrnrlat=grid["area"][0],
-                 urcrnrlon=grid["area"][3],
-                 urcrnrlat=grid["area"][1],
-                 resolution='l', ax=ax)
-
-    linear = np.load(os.path.join(result_dir, "linear-{}.npz".format(field)))
-    homogeneous = np.load(os.path.join(result_dir,
-                                       "homogeneous-{}.npz".format(field)))
-
-    x, y = bm(linear["lon"], linear["lat"])
-    shape = linear["shape"]
-    differences = linear["result"] - homogeneous["result"]
-    vmax = np.abs(differences).max()
-
-    cb = bm.contourf(x.reshape(shape), y.reshape(shape), differences.reshape(shape),
-                     100, vmax=vmax, vmin=-vmax, cmap="RdBu_r")
-    bm.contour(x.reshape(shape), y.reshape(shape), differences.reshape(shape),
-               5, colors='k', linewidths=0.7)
-    bm.drawcountries()
-    bm.drawstates()
-    bm.drawcoastlines()
-    bm.drawmeridians(np.arange(-80, -50, 2),
-                     labels=[False, False, False, True])
-    bm.drawparallels(np.arange(-50, -30, 2),
-                     labels=[True, False, False, False])
-    plt.colorbar(cb, ax=ax, label=labels[field])
-    ax.set_title(titles[field])
-
-plt.show()
-
-# Exponential and Homogeneous
-fig, axes = plt.subplots(1, 2, figsize=(15, 7))
-axes = axes.ravel()
-for field, ax in zip(fields, axes):
-
-    bm = Basemap(projection='merc',
-                 llcrnrlon=grid["area"][2],
-                 llcrnrlat=grid["area"][0],
-                 urcrnrlon=grid["area"][3],
-                 urcrnrlat=grid["area"][1],
-                 resolution='l', ax=ax)
-
-    exponential = np.load(os.path.join(result_dir, "exponential-{}.npz".format(field)))
-    homogeneous = np.load(os.path.join(result_dir,
-                                       "homogeneous-{}.npz".format(field)))
-
-    x, y = bm(exponential["lon"], exponential["lat"])
-    shape = exponential["shape"]
-    differences = exponential["result"] - homogeneous["result"]
-    vmax = np.abs(differences).max()
-
-    cb = bm.contourf(x.reshape(shape), y.reshape(shape), differences.reshape(shape),
-                     100, vmax=vmax, vmin=-vmax, cmap="RdBu_r")
-    bm.contour(x.reshape(shape), y.reshape(shape), differences.reshape(shape),
-               5, colors='k', linewidths=0.7)
-    bm.drawcountries()
-    bm.drawstates()
-    bm.drawcoastlines()
-    bm.drawmeridians(np.arange(-80, -50, 2),
-                     labels=[False, False, False, True])
-    bm.drawparallels(np.arange(-50, -30, 2),
-                     labels=[True, False, False, False])
-    plt.colorbar(cb, ax=ax, label=labels[field])
-    ax.set_title(titles[field])
-
+grid.tight_layout(fig)
 plt.show()
