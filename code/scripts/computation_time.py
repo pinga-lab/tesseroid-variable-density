Fix wrong computation time calculation

code/scripts/computation_time.py
@@ -73,7 +73,7 @@ if compute:
                 start_time = time.time()
                 getattr(tesseroid, field)(lon, lat, height, model)
                 end_time = time.time()
-                times[run] = start_time - end_time
+                times[run] = end_time - start_time
             times = np.mean(times)
             df.at[height, "{}_{}".format(density, field)] = times
 
@@ -99,7 +99,7 @@ if compute:
                 start_time = time.time()
                 getattr(tesseroid, field)(lon, lat, height, model)
                 end_time = time.time()
-                times[run] = start_time - end_time
+                times[run] = end_time - start_time
             times = np.mean(times)
             df.at[height, "{}_{}".format(density, field)] = times
 